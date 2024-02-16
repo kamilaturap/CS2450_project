e3b50b960a93eb23b2f73e07abc080fff452de0e
CamHur <112679698+CamHur@users.noreply.github.com>
2024-02-15 21:13:12 -0700

    Update main.py

--- Milestone3/main.py
+++ Milestone3/main.py
@@ -25,109 +25,60 @@ def main():
     while prog.pointer <= 99 and operation != '43':
         if len(prog.registers[prog.pointer]) == 5:
             operation = prog.registers[prog.pointer][1:3]
-            
-            match operation:
-            #i/o operations:
-                #READ
-                case '10':
-                    prog.read(prog.registers[prog.pointer][3:])
-                #WRTIE
-                case '11':
-                    prog.write(prog.registers[prog.pointer][3:])
-            #load/store operations:
-                #LOAD
-                case '20':
-                    prog.load(prog.registers[prog.pointer][3:])
-                #STORE
-                case '21':
-                    prog.store(prog.registers[prog.pointer][3:])
-            #Arithmetic operation:
-                #ADD
-                case '30':
-                    prog.add(prog.registers[prog.pointer][3:])
-                #SUBTRACT
-                case '31':
-                    prog.subtract(prog.registers[prog.pointer][3:])
-                #DIVIDE
-                case '32':
-                    prog.divide(prog.registers[prog.pointer][3:])
-                #MULTIPLY
-                case '33':
-                    prog.multiply(prog.registers[prog.pointer][3:])
-
-            #Control operation:
-                #BRANCH
-                case '40':
-                    prog.branch(prog.registers[prog.pointer][3:])
-                #BRANCHNEG
-                case '41':
-                    prog.branchneg(prog.registers[prog.pointer][3:])
-                #BRANCHZERO
-                case '42':
-                    prog.branchzero(prog.registers[prog.pointer][3:])          
-                #HALT
-                case '43':
-                    break
-
-                #default:
-                case _:
-                    prog.pointer += 1
-                    continue
-            prog.pointer += 1
+            mem = int(prog.registers[prog.pointer][3:])
 
         elif len(prog.registers[prog.pointer]) == 4:
             operation = prog.registers[prog.pointer][0:2]
+            mem = int(prog.registers[prog.pointer][2:])
+            
+        match operation:
+        #i/o operations:
+            #READ
+            case '10':
+                prog.read(mem)
+            #WRTIE
+            case '11':
+                prog.write(mem)
+        #load/store operations:
+            #LOAD
+            case '20':
+                prog.load(mem)
+            #STORE
+            case '21':
+                prog.store(mem)
+        #Arithmetic operation:
+            #ADD
+            case '30':
+                prog.add(mem)
+            #SUBTRACT
+            case '31':
+                prog.subtract(mem)
+            #DIVIDE
+            case '32':
+                prog.divide(mem)
+            #MULTIPLY
+            case '33':
+                prog.multiply(mem)
 
-            match operation:
-            #i/o operations:
-                #READ
-                case '10':
-                    prog.read(prog.registers[prog.pointer][2:])
-                #WRTIE
-                case '11':
-                    prog.write(prog.registers[prog.pointer][2:])
+        #Control operation:
+            #BRANCH
+            case '40':
+                prog.branch(mem)
+            #BRANCHNEG
+            case '41':
+                prog.branchneg(mem)
+            #BRANCHZERO
+            case '42':
+                prog.branchzero(mem)          
+            #HALT
+            case '43':
+                break
 
-            #load/store operations:
-                #LOAD
-                case '20':
-                    prog.load(prog.registers[prog.pointer][2:])
-                #STORE
-                case '21':
-                    prog.store(prog.registers[prog.pointer][2:])
-
-            #Arithmetic operation:
-                #ADD
-                case '30':
-                    prog.add(prog.registers[prog.pointer][2:])
-                #SUBTRACT
-                case '31':
-                    prog.subtract(prog.registers[prog.pointer][2:])
-                #DIVIDE
-                case '32':
-                    prog.divide(prog.registers[prog.pointer][2:])
-                #MULTIPLY
-                case '33':
-                    prog.multiply(prog.registers[prog.pointer][2:])
-
-            #Control operation:
-                #BRANCH
-                case '40':
-                    prog.branch(prog.registers[prog.pointer][2:])
-                #BRANCHNEG
-                case '41':
-                    prog.branchneg(prog.registers[prog.pointer][2:])                        
-                #BRANCHZERO
-                case '42':
-                    prog.branchzero(prog.registers[prog.pointer][2:])        
-                #HALT
-                case '43':
-                    break
-
-                #default:
-                case _:
-                    prog.pointer += 1
-                    continue
-            prog.pointer += 1
+            #default:
+            case _:
+                prog.pointer += 1
+                continue
+        prog.pointer += 1
     return
 
 if __name__ == "__main__":
