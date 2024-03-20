51a9a056550b3e124a2e31b0a9f875d53a18dce5
kamila <kamila.turapova@gmail.com>
2024-03-20 15:30:46 -0600

    more stuff

--- Milestone4/File.py
+++ Milestone4/File.py
@@ -10,7 +10,7 @@ class FileManager:
     def RunFile(self, prog, gui):
         text = gui.progField.get('1.0', tk.END)
         lst = text.split("\n")
-        if len(lst) > 99:
+        if len(lst) > 100:
             gui.invalid_input()
         else:
             prog.readProgram(text)
