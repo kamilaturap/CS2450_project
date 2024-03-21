461ff29e4d090ec169109c07d0f60a49240fef37
Mackay Grange <mackay.grange@gmail.com>
2024-03-21 12:46:33 -0600

    color functionality

--- Milestone4/experimental_gui.py
+++ Milestone4/experimental_gui.py
@@ -141,17 +141,152 @@ class MainScreen(tk.Frame):
         settings_button.grid(row=0, column=0,)
 
 
-def update_primary_color(screen, hex_value:str) -> None:
+def to_hex(value: int) -> str:
+
+    hex_value = hex(value)[2:]
+
+    if len(hex_value) == 1:
+        hex_value = '0' + hex_value
+    return hex_value
+
+
+def update_primary_color(screen, hex_value: str, red_value: int, green_value: int, blue_value: int) -> None:
+
+    current_color = screen.controller.primary_color
+    current_red = int(current_color[1:3], 16)
+    current_green = int(current_color[3:5], 16)
+    current_blue = int(current_color[5:7], 16)
+
+    if hex_value == "":
+        red_value = to_hex(red_value)
+        green_value = to_hex(green_value)
+        blue_value = to_hex(blue_value)
+
+        hex_value = red_value + green_value + blue_value
+
+        screen.hex_text_box_0.insert(0, hex_value)
+
+    elif current_red != red_value or current_green != green_value or current_blue != blue_value:
+
+        red_value = to_hex(red_value)
+        green_value = to_hex(green_value)
+        blue_value = to_hex(blue_value)
+
+        hex_value = red_value + green_value + blue_value
+
+        screen.hex_text_box_0.delete(0, "end")
+        screen.hex_text_box_0.insert(0, hex_value)
+
+    else:
+        red_value = int(hex_value[0:2], 16)
+        green_value = int(hex_value[2:4], 16)
+        blue_value = int(hex_value[4:6], 16)
+
+        screen.red_slider_0.set(red_value)
+        screen.green_slider_0.set(green_value)
+        screen.blue_slider_0.set(blue_value)
 
     hex_value = "#" + hex_value
 
     screen.controller.primary_color = hex_value
 
+    screen.configure(bg=hex_value)
     screen.subframe_0.configure(bg=hex_value)
     screen.subframe_1.configure(bg=hex_value)
     screen.subframe_1_0.configure(bg=hex_value)
+    screen.subframe_1_1.configure(bg=hex_value)
     screen.subframe_2.configure(bg=hex_value)
-    screen.subframe_x.configure(bg=hex_value)
+    screen.subframe_2_0.configure(bg=hex_value)
+    screen.subframe_2_1.configure(bg=hex_value)
+    screen.subframe_3.configure(bg=hex_value)
+
+    screen.rgb_label.configure(bg=hex_value)
+    screen.primary_label.configure(bg=hex_value)
+    screen.red_label_0.configure(bg=hex_value)
+    screen.green_label_0.configure(bg=hex_value)
+    screen.blue_label_0.configure(bg=hex_value)
+    screen.hex_label_0.configure(bg=hex_value)
+    screen.secondary_label.configure(bg=hex_value)
+    screen.red_label_1.configure(bg=hex_value)
+    screen.green_label_1.configure(bg=hex_value)
+    screen.blue_label_1.configure(bg=hex_value)
+    screen.hex_label_1.configure(bg=hex_value)
+
+    screen.red_slider_0.configure(bg=hex_value)
+    screen.green_slider_0.configure(bg=hex_value)
+    screen.blue_slider_0.configure(bg=hex_value)
+    screen.red_slider_1.configure(bg=hex_value)
+    screen.green_slider_1.configure(bg=hex_value)
+    screen.blue_slider_1.configure(bg=hex_value)
+
+    screen.apply_button_0.configure(fg=hex_value)
+    screen.apply_button_1.configure(fg=hex_value)
+    screen.done_button.configure(fg=hex_value)
+
+
+def update_secondary_color(screen, hex_value: str, red_value: int, green_value: int, blue_value: int) -> None:
+
+    current_color = screen.controller.secondary_color
+    current_red = int(current_color[1:3], 16)
+    current_green = int(current_color[3:5], 16)
+    current_blue = int(current_color[5:7], 16)
+
+    if hex_value == "":
+        red_value = to_hex(red_value)
+        green_value = to_hex(green_value)
+        blue_value = to_hex(blue_value)
+
+        hex_value = red_value + green_value + blue_value
+
+        screen.hex_text_box_1.insert(0, hex_value)
+
+    elif current_red != red_value or current_green != green_value or current_blue != blue_value:
+
+        red_value = to_hex(red_value)
+        green_value = to_hex(green_value)
+        blue_value = to_hex(blue_value)
+
+        hex_value = red_value + green_value + blue_value
+
+        screen.hex_text_box_1.delete(0, "end")
+        screen.hex_text_box_1.insert(0, hex_value)
+
+    else:
+        red_value = int(hex_value[0:2], 16)
+        green_value = int(hex_value[2:4], 16)
+        blue_value = int(hex_value[4:6], 16)
+
+        screen.red_slider_1.set(red_value)
+        screen.green_slider_1.set(green_value)
+        screen.blue_slider_1.set(blue_value)
+
+    hex_value = "#" + hex_value
+
+    screen.controller.secondary_color = hex_value
+
+    screen.rgb_label.configure(fg=hex_value)
+    screen.primary_label.configure(fg=hex_value)
+    screen.red_label_0.configure(fg=hex_value)
+    screen.green_label_0.configure(fg=hex_value)
+    screen.blue_label_0.configure(fg=hex_value)
+    screen.hex_label_0.configure(fg=hex_value)
+    screen.secondary_label.configure(fg=hex_value)
+    screen.red_label_1.configure(fg=hex_value)
+    screen.green_label_1.configure(fg=hex_value)
+    screen.blue_label_1.configure(fg=hex_value)
+    screen.hex_label_1.configure(fg=hex_value)
+
+    screen.red_slider_0.configure(fg=hex_value)
+    screen.green_slider_0.configure(fg=hex_value)
+    screen.blue_slider_0.configure(fg=hex_value)
+    screen.red_slider_1.configure(fg=hex_value)
+    screen.green_slider_1.configure(fg=hex_value)
+    screen.blue_slider_1.configure(fg=hex_value)
+
+    screen.apply_button_0.configure(bg=hex_value)
+    screen.apply_button_1.configure(bg=hex_value)
+    screen.done_button.configure(bg=hex_value)
+
 
 class SettingsScreen(tk.Frame):
 
@@ -186,6 +321,7 @@ class SettingsScreen(tk.Frame):
         self.red_label_0.grid(row=0, column=0)
 
         self.red_slider_0 = tk.Scale(self.subframe_1_0, from_=0, to=255, bd=0, highlightthickness=0, length=255, bg=controller.primary_color, fg=BG_COLOR_GREY_DEFAULT, orient=tk.HORIZONTAL)
+        self.red_slider_0.set(int(self.controller.primary_color[1:3], 16))
         self.red_slider_0.grid(row=0, column=1)
 
         self.green_label_0 = tk.Label(self.subframe_1_0, text="Green Value:", bg=controller.primary_color, fg=BG_COLOR_GREY_DEFAULT)
@@ -193,6 +329,7 @@ class SettingsScreen(tk.Frame):
         self.green_label_0.grid(row=1, column=0)
 
         self.green_slider_0 = tk.Scale(self.subframe_1_0, from_=0, to=255, bd=0, highlightthickness=0, length=255, bg=controller.primary_color, fg=BG_COLOR_GREY_DEFAULT, orient=tk.HORIZONTAL)
+        self.green_slider_0.set(int(self.controller.primary_color[3:5], 16))
         self.green_slider_0.grid(row=1, column=1)
 
         self.blue_label_0 = tk.Label(self.subframe_1_0, text="Blue Value:", bg=controller.primary_color, fg=BG_COLOR_GREY_DEFAULT)
@@ -200,6 +337,7 @@ class SettingsScreen(tk.Frame):
         self.blue_label_0.grid(row=2, column=0)
 
         self.blue_slider_0 = tk.Scale(self.subframe_1_0, from_=0, to=255, bd=0, highlightthickness=0, length=255, bg=controller.primary_color, fg=BG_COLOR_GREY_DEFAULT, orient=tk.HORIZONTAL)
+        self.blue_slider_0.set(int(self.controller.primary_color[5:7], 16))
         self.blue_slider_0.grid(row=2, column=1)
 
         self.subframe_1_1 = tk.LabelFrame(self.subframe_1, padx=5, pady=5, bd=0)
@@ -211,9 +349,10 @@ class SettingsScreen(tk.Frame):
         self.hex_label_0.grid(row=0, column=0)
 
         self.hex_text_box_0 = tk.Entry(self.subframe_1_1, width=10, bg=BG_COLOR_GREY_DEFAULT)
+        self.hex_text_box_0.insert(0, self.controller.primary_color[1:])
         self.hex_text_box_0.grid(row=0, column=1)
 
-        self.apply_button_0 = tk.Button(self.subframe_1_1, text="Apply", bg=BG_COLOR_GREY_DEFAULT, fg=controller.primary_color, width=10, command=lambda: update_primary_color(self, str(self.hex_text_box_0.get())))
+        self.apply_button_0 = tk.Button(self.subframe_1_1, text="Apply", bg=BG_COLOR_GREY_DEFAULT, fg=controller.primary_color, width=10, command=lambda: update_primary_color(self, str(self.hex_text_box_0.get()), int(self.red_slider_0.get()), int(self.green_slider_0.get()), int(self.blue_slider_0.get())))
         self.apply_button_0.configure(font=FONT_TUPLE_REG)
         self.apply_button_0.grid(row=0, column=2, padx=10)
 
@@ -234,6 +373,7 @@ class SettingsScreen(tk.Frame):
         self.red_label_1.grid(row=0, column=0)
 
         self.red_slider_1 = tk.Scale(self.subframe_2_0, from_=0, to=255, bd=0, highlightthickness=0, length=255, bg=controller.primary_color, fg=BG_COLOR_GREY_DEFAULT, orient=tk.HORIZONTAL)
+        self.red_slider_1.set(int(self.controller.secondary_color[1:3], 16))
         self.red_slider_1.grid(row=0, column=1)
 
         self.green_label_1 = tk.Label(self.subframe_2_0, text="Green Value:", bg=controller.primary_color, fg=BG_COLOR_GREY_DEFAULT)
@@ -241,6 +381,7 @@ class SettingsScreen(tk.Frame):
         self.green_label_1.grid(row=1, column=0)
 
         self.green_slider_1 = tk.Scale(self.subframe_2_0, from_=0, to=255, bd=0, highlightthickness=0, length=255, bg=controller.primary_color, fg=BG_COLOR_GREY_DEFAULT, orient=tk.HORIZONTAL)
+        self.green_slider_1.set(int(self.controller.secondary_color[3:5], 16))
         self.green_slider_1.grid(row=1, column=1)
 
         self.blue_label_1 = tk.Label(self.subframe_2_0, text="Blue Value:", bg=controller.primary_color, fg=BG_COLOR_GREY_DEFAULT)
@@ -248,6 +389,7 @@ class SettingsScreen(tk.Frame):
         self.blue_label_1.grid(row=2, column=0)
 
         self.blue_slider_1 = tk.Scale(self.subframe_2_0, from_=0, to=255, bd=0, highlightthickness=0, length=255, bg=controller.primary_color, fg=BG_COLOR_GREY_DEFAULT, orient=tk.HORIZONTAL)
+        self.blue_slider_1.set(int(self.controller.secondary_color[5:7], 16))
         self.blue_slider_1.grid(row=2, column=1)
 
         self.subframe_2_1 = tk.LabelFrame(self.subframe_2, padx=5, pady=5, bd=0)
@@ -259,17 +401,18 @@ class SettingsScreen(tk.Frame):
         self.hex_label_1.grid(row=0, column=0)
 
         self.hex_text_box_1 = tk.Entry(self.subframe_2_1, width=10, bg=BG_COLOR_GREY_DEFAULT)
+        self.hex_text_box_1.insert(0, self.controller.secondary_color[1:])
         self.hex_text_box_1.grid(row=0, column=1)
 
-        self.apply_button_1 = tk.Button(self.subframe_2_1, text="Apply", bg=BG_COLOR_GREY_DEFAULT, fg=controller.primary_color, width=10, command=lambda: controller.show_frame("MainScreen"))
+        self.apply_button_1 = tk.Button(self.subframe_2_1, text="Apply", bg=BG_COLOR_GREY_DEFAULT, fg=controller.primary_color, width=10, command=lambda: update_secondary_color(self, str(self.hex_text_box_1.get()), int(self.red_slider_1.get()), int(self.green_slider_1.get()), int(self.blue_slider_1.get())))
         self.apply_button_1.configure(font=FONT_TUPLE_REG)
         self.apply_button_1.grid(row=0, column=2, padx=10)
 
-        self.subframe_x = tk.LabelFrame(self, padx=5, pady=5, bd=0)
-        self.subframe_x.configure(bg=controller.primary_color)
-        self.subframe_x.pack()
+        self.subframe_3 = tk.LabelFrame(self, padx=5, pady=5, bd=0)
+        self.subframe_3.configure(bg=controller.primary_color)
+        self.subframe_3.pack()
 
-        self.done_button = tk.Button(self.subframe_x, text="Done", bg=BG_COLOR_GREY_DEFAULT, fg=controller.primary_color, width=25, command=lambda: controller.show_frame("MainScreen"))
+        self.done_button = tk.Button(self.subframe_3, text="Done", bg=BG_COLOR_GREY_DEFAULT, fg=controller.primary_color, width=25, command=lambda: controller.show_frame("MainScreen"))
         self.done_button.configure(font=FONT_TUPLE_REG)
         self.done_button.grid(row=0, column=0, pady=10)
 
