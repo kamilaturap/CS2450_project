42f739b5db8339270a08ecd1955d26870c9ecda2
Mackay Grange <mackay.grange@gmail.com>
2024-03-21 14:34:10 -0600

    Adding use cases for Mackay's functions.

--- Milestone4/experimental_gui.py
+++ Milestone4/experimental_gui.py
@@ -51,94 +51,94 @@ class MainScreen(tk.Frame):
 
         controller.title("UVSim")
 
-        subframe_0 = tk.LabelFrame(self, padx=5, pady=5, bd=0)
-        subframe_0.configure(bg=controller.primary_color)
-        subframe_0.pack()
+        self.subframe_0 = tk.LabelFrame(self, padx=5, pady=5, bd=0)
+        self.subframe_0.configure(bg=controller.primary_color)
+        self.subframe_0.pack()
 
-        uvsim_label = tk.Label(subframe_0, text="UVSim", bg=controller.primary_color, fg=BG_COLOR_GREY_DEFAULT)
-        uvsim_label.configure(font=FONT_TUPLE_TITLE)
-        uvsim_label.grid(row=0, column=0)
+        self.uvsim_label = tk.Label(self.subframe_0, text="UVSim", bg=controller.primary_color, fg=BG_COLOR_GREY_DEFAULT)
+        self.uvsim_label.configure(font=FONT_TUPLE_TITLE)
+        self.uvsim_label.grid(row=0, column=0)
 
-        subframe_1 = tk.LabelFrame(self, padx=5, pady=5, bd=0)
-        subframe_1.configure(bg=controller.primary_color)
-        subframe_1.pack()
+        self.subframe_1 = tk.LabelFrame(self, padx=5, pady=5, bd=0)
+        self.subframe_1.configure(bg=controller.primary_color)
+        self.subframe_1.pack()
 
-        subframe_1_0 = tk.LabelFrame(subframe_1, padx=5, pady=5, bd=0)
-        subframe_1_0.configure(bg=controller.primary_color)
-        subframe_1_0.grid(row=0, column=0)
+        self.subframe_1_0 = tk.LabelFrame(self.subframe_1, padx=5, pady=5, bd=0)
+        self.subframe_1_0.configure(bg=controller.primary_color)
+        self.subframe_1_0.grid(row=0, column=0)
 
-        subframe_1_1 = tk.LabelFrame(subframe_1_0, padx=5, pady=5, bd=0)
-        subframe_1_1.configure(bg=controller.primary_color)
-        subframe_1_1.pack()
+        self.subframe_1_0_0 = tk.LabelFrame(self.subframe_1_0, padx=5, pady=5, bd=0)
+        self.subframe_1_0_0.configure(bg=controller.primary_color)
+        self.subframe_1_0_0.pack()
 
-        import_button = tk.Button(subframe_1_1, text="Import .txt File", bg=BG_COLOR_GREY_DEFAULT, fg=controller.primary_color)
-        import_button.configure(font=FONT_TUPLE_REG)
-        import_button.grid(row=0, column=0, padx=5)
+        self.import_button = tk.Button(self.subframe_1_0_0, text="Import .txt File", bg=BG_COLOR_GREY_DEFAULT, fg=controller.primary_color)
+        self.import_button.configure(font=FONT_TUPLE_REG)
+        self.import_button.grid(row=0, column=0, padx=5)
 
-        selected_file_label = tk.Label(subframe_1_1, text="Selected File: ", bg=controller.primary_color, fg=BG_COLOR_GREY_DEFAULT)
-        selected_file_label.configure(font=FONT_TUPLE_REG)
-        selected_file_label.grid(row=0, column=1, padx=5)
+        self.selected_file_label = tk.Label(self.subframe_1_0_0, text="Selected File: ", bg=controller.primary_color, fg=BG_COLOR_GREY_DEFAULT)
+        self.selected_file_label.configure(font=FONT_TUPLE_REG)
+        self.selected_file_label.grid(row=0, column=1, padx=5)
 
-        subframe_1_0_1 = tk.LabelFrame(subframe_1_0, padx=5, pady=5, bd=0)
-        subframe_1_0_1.configure(bg=controller.primary_color)
-        subframe_1_0_1.pack()
+        self.subframe_1_0_1 = tk.LabelFrame(self.subframe_1_0, padx=5, pady=5, bd=0)
+        self.subframe_1_0_1.configure(bg=controller.primary_color)
+        self.subframe_1_0_1.pack()
 
-        selected_file_text_box = tk.Entry(subframe_1_0_1, width=35, bg=BG_COLOR_GREY_DEFAULT)
-        selected_file_text_box.grid(row=0, column=0, padx=5)
+        self.selected_file_text_box = tk.Entry(self.subframe_1_0_1, width=35, bg=BG_COLOR_GREY_DEFAULT)
+        self.selected_file_text_box.grid(row=0, column=0, padx=5)
 
-        subframe_1_1 = tk.LabelFrame(subframe_1, padx=5, pady=5, bd=0)
-        subframe_1_1.configure(bg=controller.primary_color)
-        subframe_1_1.grid(row=0, column=1)
+        self.subframe_1_1 = tk.LabelFrame(self.subframe_1, padx=5, pady=5, bd=0)
+        self.subframe_1_1.configure(bg=controller.primary_color)
+        self.subframe_1_1.grid(row=0, column=1)
 
-        load_button = tk.Button(subframe_1_1, text="Load Selected Program", bg=BG_COLOR_GREY_DEFAULT, fg=controller.primary_color, width=25)
-        load_button.configure(font=FONT_TUPLE_REG)
-        load_button.grid(row=0, column=0, pady=5)
+        self.load_button = tk.Button(self.subframe_1_1, text="Load Selected Program", bg=BG_COLOR_GREY_DEFAULT, fg=controller.primary_color, width=25)
+        self.load_button.configure(font=FONT_TUPLE_REG)
+        self.load_button.grid(row=0, column=0, pady=5)
 
-        save_button = tk.Button(subframe_1_1, text="Save Program Changes", bg=BG_COLOR_GREY_DEFAULT, fg=controller.primary_color, width=25)
-        save_button.configure(font=FONT_TUPLE_REG)
-        save_button.grid(row=1, column=0, pady=5)
+        self.save_button = tk.Button(self.subframe_1_1, text="Save Program Changes", bg=BG_COLOR_GREY_DEFAULT, fg=controller.primary_color, width=25)
+        self.save_button.configure(font=FONT_TUPLE_REG)
+        self.save_button.grid(row=1, column=0, pady=5)
 
-        subframe_1_2 = tk.LabelFrame(subframe_1, padx=5, pady=5, bd=0)
-        subframe_1_2.configure(bg=controller.primary_color)
-        subframe_1_2.grid(row=0, column=2)
+        self.subframe_1_2 = tk.LabelFrame(self.subframe_1, padx=5, pady=5, bd=0)
+        self.subframe_1_2.configure(bg=controller.primary_color)
+        self.subframe_1_2.grid(row=0, column=2)
 
-        run_button = tk.Button(subframe_1_2, text="Run Program", bg=BG_COLOR_GREY_DEFAULT, fg=controller.primary_color, width=25)
-        run_button.configure(font=FONT_TUPLE_REG)
-        run_button.grid(row=1, column=0)
+        self.run_button = tk.Button(self.subframe_1_2, text="Run Program", bg=BG_COLOR_GREY_DEFAULT, fg=controller.primary_color, width=25)
+        self.run_button.configure(font=FONT_TUPLE_REG)
+        self.run_button.grid(row=1, column=0)
 
-        subframe_2 = tk.LabelFrame(self, padx=0, pady=5, bd=0)
-        subframe_2.configure(bg=controller.primary_color)
-        subframe_2.pack()
+        self.subframe_2 = tk.LabelFrame(self, padx=0, pady=5, bd=0)
+        self.subframe_2.configure(bg=controller.primary_color)
+        self.subframe_2.pack()
 
-        scroll_y = tk.Scrollbar(subframe_2)
-        text_area_program = tk.Text(subframe_2, bg=BG_COLOR_WHITE_DEFAULT, bd=5, font=FONT_TUPLE_TEXT, relief="ridge", yscrollcommand=scroll_y.set, width=88, height=10)
-        text_area_program.grid(row=0, column=0, sticky=tk.N+tk.S+tk.W+tk.E, pady=10)
-        scroll_y.grid(row=0, column=1, sticky=tk.N+tk.S, pady=10)
+        self.scroll_y = tk.Scrollbar(self.subframe_2)
+        self.text_area_program = tk.Text(self.subframe_2, bg=BG_COLOR_WHITE_DEFAULT, bd=5, font=FONT_TUPLE_TEXT, relief="ridge", yscrollcommand=self.scroll_y.set, width=88, height=10)
+        self.text_area_program.grid(row=0, column=0, sticky=tk.N+tk.S+tk.W+tk.E, pady=10)
+        self.scroll_y.grid(row=0, column=1, sticky=tk.N+tk.S, pady=10)
 
-        subframe_3 = tk.LabelFrame(self, padx=5, pady=5, bd=0)
-        subframe_3.configure(bg=controller.primary_color)
-        subframe_3.pack()
+        self.subframe_3 = tk.LabelFrame(self, padx=5, pady=5, bd=0)
+        self.subframe_3.configure(bg=controller.primary_color)
+        self.subframe_3.pack()
 
-        output_label = tk.Label(subframe_3, text="Output: ", bg=controller.primary_color, fg=BG_COLOR_GREY_DEFAULT)
-        output_label.configure(font=FONT_TUPLE_REG)
-        output_label.grid(row=0, column=0)
+        self.output_label = tk.Label(self.subframe_3, text="Output: ", bg=controller.primary_color, fg=BG_COLOR_GREY_DEFAULT)
+        self.output_label.configure(font=FONT_TUPLE_REG)
+        self.output_label.grid(row=0, column=0)
 
-        subframe_4 = tk.LabelFrame(self, padx=5, pady=5, bd=0)
-        subframe_4.configure(bg=controller.primary_color)
-        subframe_4.pack()
+        self.subframe_4 = tk.LabelFrame(self, padx=5, pady=5, bd=0)
+        self.subframe_4.configure(bg=controller.primary_color)
+        self.subframe_4.pack()
 
-        scroll_y_2 = tk.Scrollbar(subframe_4)
-        text_area_output = tk.Text(subframe_4, bg=BG_COLOR_WHITE_DEFAULT, bd=5, font=FONT_TUPLE_TEXT, relief="ridge", yscrollcommand=scroll_y_2.set, width=88, height=10)
-        text_area_output.grid(row=0, column=0, sticky=tk.N+tk.S+tk.W+tk.E, pady=10)
-        scroll_y_2.grid(row=0, column=1, sticky=tk.N+tk.S, pady=10)
+        self.scroll_y_2 = tk.Scrollbar(self.subframe_4)
+        self.text_area_output = tk.Text(self.subframe_4, bg=BG_COLOR_WHITE_DEFAULT, bd=5, font=FONT_TUPLE_TEXT, relief="ridge", yscrollcommand=self.scroll_y_2.set, width=88, height=10)
+        self.text_area_output.grid(row=0, column=0, sticky=tk.N+tk.S+tk.W+tk.E, pady=10)
+        self.scroll_y_2.grid(row=0, column=1, sticky=tk.N+tk.S, pady=10)
 
-        subframe_5 = tk.LabelFrame(self, padx=5, pady=5, bd=0)
-        subframe_5.configure(bg=controller.primary_color)
-        subframe_5.pack()
+        self.subframe_5 = tk.LabelFrame(self, padx=5, pady=5, bd=0)
+        self.subframe_5.configure(bg=controller.primary_color)
+        self.subframe_5.pack()
 
-        settings_button = tk.Button(subframe_5, text="Settings", bg=BG_COLOR_GREY_DEFAULT, fg=controller.primary_color, width=25, command=lambda: controller.show_frame("SettingsScreen"))
-        settings_button.configure(font=FONT_TUPLE_REG)
-        settings_button.grid(row=0, column=0,)
+        self.settings_button = tk.Button(self.subframe_5, text="Settings", bg=BG_COLOR_GREY_DEFAULT, fg=controller.primary_color, width=25, command=lambda: controller.show_frame("SettingsScreen"))
+        self.settings_button.configure(font=FONT_TUPLE_REG)
+        self.settings_button.grid(row=0, column=0)
 
 
 def to_hex(value: int) -> str:
@@ -223,6 +223,36 @@ def update_primary_color(screen, hex_value: str, red_value: int, green_value: in
     screen.apply_button_1.configure(fg=hex_value)
     screen.done_button.configure(fg=hex_value)
 
+    screen.controller.frames["MainScreen"].configure(bg=hex_value)
+
+    screen.controller.frames["MainScreen"].subframe_0.configure(bg=hex_value)
+    screen.controller.frames["MainScreen"].subframe_1.configure(bg=hex_value)
+    screen.controller.frames["MainScreen"].subframe_1_0.configure(bg=hex_value)
+    screen.controller.frames["MainScreen"].subframe_1_0_0.configure(bg=hex_value)
+    screen.controller.frames["MainScreen"].subframe_1_0_1.configure(bg=hex_value)
+    screen.controller.frames["MainScreen"].subframe_1_1.configure(bg=hex_value)
+    screen.controller.frames["MainScreen"].subframe_1_2.configure(bg=hex_value)
+    screen.controller.frames["MainScreen"].subframe_2.configure(bg=hex_value)
+    screen.controller.frames["MainScreen"].subframe_3.configure(bg=hex_value)
+    screen.controller.frames["MainScreen"].subframe_4.configure(bg=hex_value)
+    screen.controller.frames["MainScreen"].subframe_5.configure(bg=hex_value)
+
+    screen.controller.frames["MainScreen"].uvsim_label.configure(bg=hex_value)
+    screen.controller.frames["MainScreen"].selected_file_label.configure(bg=hex_value)
+    screen.controller.frames["MainScreen"].output_label.configure(bg=hex_value)
+
+    screen.controller.frames["MainScreen"].text_area_program.configure(fg=hex_value)
+    screen.controller.frames["MainScreen"].text_area_output.configure(fg=hex_value)
+
+    screen.controller.frames["MainScreen"].selected_file_text_box.configure(fg=hex_value)
+
+    screen.controller.frames["MainScreen"].import_button.configure(fg=hex_value)
+    screen.controller.frames["MainScreen"].save_button.configure(fg=hex_value)
+    screen.controller.frames["MainScreen"].load_button.configure(fg=hex_value)
+    screen.controller.frames["MainScreen"].run_button.configure(fg=hex_value)
+    screen.controller.frames["MainScreen"].settings_button.configure(fg=hex_value)
+
+
 
 def update_secondary_color(screen, hex_value: str, red_value: int, green_value: int, blue_value: int) -> None:
 
@@ -286,6 +316,21 @@ def update_secondary_color(screen, hex_value: str, red_value: int, green_value: 
     screen.apply_button_0.configure(bg=hex_value)
     screen.apply_button_1.configure(bg=hex_value)
     screen.done_button.configure(bg=hex_value)
+
+    screen.controller.frames["MainScreen"].uvsim_label.configure(fg=hex_value)
+    screen.controller.frames["MainScreen"].selected_file_label.configure(fg=hex_value)
+    screen.controller.frames["MainScreen"].output_label.configure(fg=hex_value)
+
+    screen.controller.frames["MainScreen"].text_area_program.configure(bg=hex_value)
+    screen.controller.frames["MainScreen"].text_area_output.configure(bg=hex_value)
+
+    screen.controller.frames["MainScreen"].selected_file_text_box.configure(bg=hex_value)
+
+    screen.controller.frames["MainScreen"].import_button.configure(bg=hex_value)
+    screen.controller.frames["MainScreen"].load_button.configure(bg=hex_value)
+    screen.controller.frames["MainScreen"].save_button.configure(bg=hex_value)
+    screen.controller.frames["MainScreen"].run_button.configure(bg=hex_value)
+    screen.controller.frames["MainScreen"].settings_button.configure(bg=hex_value)
 
 
 class SettingsScreen(tk.Frame):
